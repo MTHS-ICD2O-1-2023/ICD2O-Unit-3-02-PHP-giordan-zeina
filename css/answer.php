7557eb68e5cbfbee0178c863b7c1e529773c1385
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-18 13:06:59 +0000

    done

---
+++ css/answer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Volume of a Triangle, in PHP" />
+  <meta name="keywords" content="mths, ics2o" />
+  <meta name="author" content="Giordan Zeina" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-purple.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Volume of a Pyramid, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Volume of a Pyramid, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/pyramid.png" alt="pyramid" width="250" />
+      </div>
+      <div class="page-content-php">
+        <div name="volume-of-pyramid">
+          <div class="mdl-layout__header-row">
+            <br />
+            <div class="mdl-layout__content">
+            </div>
+            <div class="mdl-layout__left">
+              <?php
+
+              // input
+              $length = $_POST["length"];
+              $width = $_POST["width"];
+              $height = $_POST["height"];
+
+              // process
+              $volume = ($length * $width * $height) / 3;
+
+              // output
+              echo "If the length is " . $length . " and the width is " . $width . " and the height is " . $height . ". ";
+              echo "<br />";
+              echo "<br />";
+              echo "Then the Volume is: " . round($volume, 2) . " mm³";
+              echo "<br />";
+
+              ?>
+            </div>
+          </div>
+        </div>
+    </main>
+  </div>
+</body>
+
+</html>
